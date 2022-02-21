21a9a0a14a437bc942cfdde947abc781937e6c0e
Piyush Dubey <piyush115dubey@gmail.com>
2022-02-21 13:32:54 +0530

    added custom scripts and workers

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,19 @@
+export async function handleRequestWithText(request: Request): Promise<Response> {
+  return new Response(`request method: ${request.method}. Hello world`)
+}
+
+const SITE_URL = `https://welcome.developers.workers.dev/wrangler-oauth-consent-granted`
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  // Only GET requests work with this proxy.
+  if (request.method !== 'GET') return MethodNotAllowed(request)
+  return fetch(SITE_URL)
+}
+function MethodNotAllowed(request: Request) {
+  return new Response(`Method ${request.method} not allowed.`, {
+    status: 405,
+    headers: {
+      'Allow': 'GET'
+    }
+  })
 }
