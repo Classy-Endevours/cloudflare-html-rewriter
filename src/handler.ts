2cbd263c48551e74da3e21360da3692dfe321555
Manthan Mehta <manthanmehta810@gmail.com>
2022-02-22 00:45:07 +0530

    Cloak-ist

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,21 @@
+export async function handleRequestWithText(request: Request): Promise<Response> {
+  return new Response(`request method: ${request.method}. Hello world`)
+}
+
+const SITE_URL_OLD = `https://welcome.developers.workers.dev/wrangler-oauth-consent-granted`
+const SITE_URL = `https://outlook.office365.com/calendar/published/b07091f33c2e4b118013d49b8d61ed5b@wpintegrate.com/6e070fb44f7c41f8abe2b9665129241d4795166312977762451/calendar.html`
+const WIKI_PEDIA = 'https://en.wikipedia.org/wiki/Main_Page'
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  // Only GET requests work with this proxy.
+  if (request.method !== 'GET') return MethodNotAllowed(request)
+  return fetch(SITE_URL)
+}
+function MethodNotAllowed(request: Request) {
+  return new Response(`Method ${request.method} not allowed.`, {
+    status: 405,
+    headers: {
+      'Allow': 'GET'
+    }
+  })
 }
---
+++ src/head.ts
@@ -0,0 +1,42 @@
+// Head rewriter to handle favicon
+class HeadRewriter {
+  element(element: Element) {
+    element.append(
+      `<link rel="icon" href="https://api.super.so/asset/super.so/55b95b71-eb85-4643-9315-8e0af7598a2f.svg">`,
+      {
+        html: true,
+      }
+    )
+  }
+}
+
+// Change HTML response to fix favicon
+async function appendHtml(res: Response) {
+  return (
+    new HTMLRewriter()
+      .on("head", new HeadRewriter())
+      .transform(res)
+  )
+}
+
+const SITE_URL_OLD = `https://welcome.developers.workers.dev/wrangler-oauth-consent-granted`
+const SITE_URL = `https://outlook.office365.com/calendar/published/b07091f33c2e4b118013d49b8d61ed5b@wpintegrate.com/6e070fb44f7c41f8abe2b9665129241d4795166312977762451/calendar.html`
+
+const config = {
+  originPage: SITE_URL_OLD
+}
+
+export async function handleRequest(request: Request) {
+  // Grab the request URL's pathname, we'll use it later
+  const url = new URL(request.url)
+  const targetPath = url.pathname
+  const parsedUrl = url.searchParams.get("url")
+ 
+  // Change request URLs to go through to the subdomain
+  let response = await fetch(`https://${config.originPage}${targetPath}`)
+
+  // We don't need to change these requests at all
+  // So we return the response of the fetch request from above
+  // immediately.
+  return appendHtml(response)
+}
