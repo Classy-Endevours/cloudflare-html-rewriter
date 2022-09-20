e4be37f9bbaa354be019f8e202f35dbcb492ab3f
Piyush Dubey <piyush115dubey@gmail.com>
2022-09-20 18:22:31 +0530

    exec added in app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import { ContainerInstanceManagementClient } from '@azure/arm-containerinstance'
 import { DefaultAzureCredential } from '@azure/identity'
 import tomlHandler from './tomlHandler'
+import { exec } from 'child_process'
 
 dotenv.config()
 
@@ -15,29 +16,40 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/publish/:id', async (req: Request, res: Response) => {
   try {
-    const subscriptionId = process.env.SUBSCRIPTION_ID ?? req.query.SUBSCRIPTION_ID as string
-    const resourceGroupName = process.env.RESOURCE_GROUP ?? req.query.RESOURCE_GROUP as string
-    const containerGroupName = process.env.CONTAINER_GROUP ?? req.query.CONTAINER_GROUP as string
-    const containerName = process.env.CONTAINER_NAME ?? req.query.CONTAINER_NAME as string
-    const credential = new DefaultAzureCredential()
-    const client = new ContainerInstanceManagementClient(
-      credential,
-      subscriptionId,
-    )
     tomlHandler.writePrefixFile(req.params.id as string)
-    const result = await client.containers.executeCommand(
-      resourceGroupName,
-      containerGroupName,
-      containerName,
-      {
-        command: `npm run publish`,
-        terminalSize: {
-          rows: 1,
-          cols: 1
-        }
-      },
-    )
-    res.status(200).json(result)
+    exec('npm run publish', (err, result) => {
+      if(err){
+        res.status(500).json(err)
+        return
+      } else {
+        res.status(200).json(result)
+        return
+      }
+    })
+
+    // const subscriptionId = process.env.SUBSCRIPTION_ID ?? req.query.SUBSCRIPTION_ID as string
+    // const resourceGroupName = process.env.RESOURCE_GROUP ?? req.query.RESOURCE_GROUP as string
+    // const containerGroupName = process.env.CONTAINER_GROUP ?? req.query.CONTAINER_GROUP as string
+    // const containerName = process.env.CONTAINER_NAME ?? req.query.CONTAINER_NAME as string
+    // const credential = new DefaultAzureCredential()
+    // const client = new ContainerInstanceManagementClient(
+    //   credential,
+    //   subscriptionId,
+    // )
+    // tomlHandler.writePrefixFile(req.params.id as string)
+    // const result = await client.containers.executeCommand(
+    //   resourceGroupName,
+    //   containerGroupName,
+    //   containerName,
+    //   {
+    //     command: `npm run publish`,
+    //     terminalSize: {
+    //       rows: 1,
+    //       cols: 1
+    //     }
+    //   },
+    // )
+    // res.status(200).json(result)
   } catch (error) {
     res.status(500).json(error)
   }
